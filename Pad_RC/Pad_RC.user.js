22cd333542c67afc568864f8a5a6ffc785777529
RouxRC <b.ooghe@gmail.com>
2016-05-30 02:29:26 +0200

    summarizepad

---
+++ Pad_RC/Pad_RC.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Pad RC
+// @namespace   https://pad.regardscitoyens.org/p/affaires-courantes
+// @include     https://pad.regardscitoyens.org/p/affaires-courantes
+// @version     1
+// @grant       none
+// ==/UserScript==
+var script = document.createElement("script");
+script.type = 'text/javascript';
+script.src = "https://bjperson.github.io/summarize-pad/summarize-pad.js";
+document.body.appendChild(script);
