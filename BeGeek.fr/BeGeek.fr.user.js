bafa265d32c7f17c8bb3432a4480cfc5fbe1e78b
RouxRC <b.ooghe@gmail.com>
2016-06-18 21:41:41 +0200

    add begeek

---
+++ BeGeek.fr/BeGeek.fr.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        BeGeek.fr
+// @namespace   begeek.fr
+// @include     http://www.begeek.fr/*
+// @grant       none
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
+// ==/UserScript==
+$("noscript").remove();
