5f1d484611e034280ee053892b80d76a31132c9b
RouxRC <b.ooghe@gmail.com>
2016-08-17 02:29:13 +0200

    fix redirect comment pages

--- Médiapart/Médiapart.user.js
+++ Médiapart/Médiapart.user.js
@@ -6,6 +6,6 @@
 // @grant       none
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // ==/UserScript==
-if (~window.location.href.indexOf("?onglet=full"))
+if (~window.location.href.indexOf("?onglet=full") || ~window.location.href.indexOf("/commentaires"))
   $(".content-pager, .content-page-full").css('display', 'none');
 else window.location = window.location.href + "?onglet=full";
