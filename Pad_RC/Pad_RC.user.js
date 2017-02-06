b3e95fe16183fa184c112661e5a117ec5cf1b959
RouxRC <b.ooghe@gmail.com>
2017-02-07 00:26:01 +0100

    more RC pads with summarize

--- Pad_RC/Pad_RC.user.js
+++ Pad_RC/Pad_RC.user.js
@@ -2,6 +2,8 @@
 // @name        Pad RC
 // @namespace   https://pad.regardscitoyens.org/p/affaires-courantes
 // @include     https://pad.regardscitoyens.org/p/affaires-courantes
+// @include     https://pad.regardscitoyens.org/p/Mails_RC
+// @include     https://pad.regardscitoyens.org/p/todo-list
 // @version     1
 // @grant       none
 // ==/UserScript==
