bd20a959846f969cbacb871d58faa148c3f2ab3e
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2017-07-12 14:09:18 +0200

    cleanup lemonde nojs header

--- LeMonde.fr/LeMonde.fr.user.js
+++ LeMonde.fr/LeMonde.fr.user.js
@@ -2,9 +2,10 @@
 // @name        LeMonde.fr
 // @namespace   lemonde.fr
 // @include     http://www.lemonde.fr/*
+// @include     http://abonnes.lemonde.fr/*
 // @grant       none
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // ==/UserScript==
-$(".article_noscript").remove();
-if ($(".ea_article").length)
+$(".article_noscript, #navigation-generale, #header-page").remove();
+if ($(".ea_article").length && !/abonnes.lemonde.fr/.test(window.location))
   window.location = window.location.href.replace(/:\/\/www\./, '://abonnes.');
